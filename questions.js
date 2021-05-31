978f8bfd5cdfc676d82143bae35979d651897abd
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-31 08:44:54 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"It's getting late. We ..... now.",
+        options:["had better go", "had better to go", "had rather go", "would better go"],
+        answer: 0
+    },
+    {
+        q:"I found it really difficult to ..... up so early.",
+        options:["be used to wake", "get used to waking", "use to wake", "get used to wake"],
+        answer: 1
+    },
+    {
+        q:"She is going to the hospital later today ..... her father.",
+        options:["so she visits", "for visit", "to visit", "for visiting"],
+        answer: 2
+    },
+    {
+        q:"There were ..... any people in the cinema yesterday.",
+        options:["less", "few", "little", "hardly"],
+        answer: 3
+    },
+    {
+        q:"The match was held ..... the bad weather.",
+        options:["despite", "even though", "although", "in spite"],
+        answer: 0
+    },
+    {
+        q:"I don't know what ..... supposed to do in a situation.",
+        options:["you", "you are", "are you", "do you"],
+        answer: 1
+    },
+    {
+     q:"It ..... to be  rude.",
+        options:["is not him", "is he doesn't like", "is not like him", "doesn't like him"],
+        answer: 2   
+    },
+    {
+        q:"They were ..... to learn Spanish at school.",
+        options:["had", "about", "-", "made"],
+        answer: 3
+    },
+    {
+        q:"Take a jacket ..... it gets cold later.",
+        options:["in case", "if", "unless", "until"],
+        answer: 0
+    },
+    {
+        q:"If it hadn't been for your help, we ..... in trouble now.",
+        options:["will be", "would be", "would have been", "were"],
+        answer: 1
+    }
+]
